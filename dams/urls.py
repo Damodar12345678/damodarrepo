818223e33d4f26a714f0acd12fb59a8ff2430082
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-08-28 20:14:41 +0530

    Update urls.py

--- dams/urls.py
+++ dams/urls.py
@@ -17,6 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from proapp import views
+from django.conf import settings
+ from django.conf.urls.static import static
+from django.conf import settings
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
