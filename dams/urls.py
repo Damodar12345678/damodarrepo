88cec74a72b346fc6af3f998055e723b695d8bd7
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-08-29 09:15:14 +0530

    Update urls.py

--- dams/urls.py
+++ dams/urls.py
@@ -19,11 +19,6 @@ from django.urls import path
 from proapp import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf import settings
-
-
-
-
 
 
 urlpatterns = [
@@ -32,5 +27,6 @@ urlpatterns = [
     path('' , views.index)
     
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
