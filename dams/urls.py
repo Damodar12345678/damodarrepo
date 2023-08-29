7baa90aaff80b61a3a00567b332cc3eaa10760e0
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-08-29 09:33:37 +0530

    Update urls.py

--- dams/urls.py
+++ dams/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('' , views.index)
-    
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
